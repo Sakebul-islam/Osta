1a0d029d96566e47ef4e21497381f4bea6bbe70b
Sakebul-islam <mdsakebul66@gmail.com>
2024-01-31 17:50:32 +0600

    create a middleWare and a function to handle error of application

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { userName, email, password } = req.body;
 
   if (
@@ -12,10 +13,10 @@ export const signup = async (req, res) => {
     email === '' ||
     password === ''
   ) {
-    res.status(400).json({ message: 'All filled are required' });
+    next(errorHandler(400, 'All filled are required'));
   }
 
-  const hashPassword = await bcryptjs.hashSync(password,10)
+  const hashPassword = await bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
     userName,
@@ -26,6 +27,6 @@ export const signup = async (req, res) => {
     const result = await newUser.save();
     res.send('Signup Successful');
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
