d3f16e77ded19115e3b9f8620220068680ce11a9
Sakebul-islam <mdsakebul66@gmail.com>
2024-01-31 12:20:33 +0600

    create pages and routes

---
+++ client/src/routes/Routes.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layout/MainLayout';
+import DashboardLayout from '../Layout/DashboardLayout';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard/Dashboard';
+import Projects from '../pages/Projects';
+import ErrorPage from '../pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/about', element: <About /> },
+      { path: '/projects', element: <Projects /> },
+    ],
+  },
+  { path: '/sign-in', element: <SignIn /> },
+  { path: '/sign-up', element: <SignUp /> },
+  {
+    path: 'dashboard',
+    element: <DashboardLayout />,
+
+    children: [
+      {
+        path: '/dashboard/dashboard',
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
+
+export default router;
