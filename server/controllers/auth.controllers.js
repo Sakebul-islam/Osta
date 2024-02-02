25c2e33ae88348ff1ddc2d068cd00c935532c8c8
Sakebul-islam <mdsakebul66@gmail.com>
2024-02-02 18:02:17 +0600

    check existingUser to add user in db

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -4,6 +4,8 @@ import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+
+  // Check if any required field is missing or empty
   if (
     !username ||
     !email ||
@@ -12,20 +14,31 @@ export const signup = async (req, res, next) => {
     email === '' ||
     password === ''
   ) {
-    next(errorHandler(400, 'All filled are required'));
+    return next(errorHandler(400, 'All fields are required'));
   }
 
-  const hashPassword = bcryptjs.hashSync(password, 10);
-
-  const newUser = new User({
-    username,
-    email,
-    password: hashPassword,
-  });
   try {
+    // Check if a user with the same username or email already exists
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return next(errorHandler(400, 'Username or email already exists'));
+    }
+
+    // Hash the password
+    const hashPassword = bcryptjs.hashSync(password, 10);
+
+    // Create a new user instance
+    const newUser = new User({
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    // Save the new user to the database
     const result = await newUser.save();
     res.json('Signup Successful');
   } catch (error) {
+    // Pass any errors to the error handling middleware
     next(error);
   }
 };
