a5085f896be2e609138b11dba378be60ead69592
Sakebul-islam <mdsakebul66@gmail.com>
2024-02-02 14:49:40 +0600

    show and hide password fetcher added on sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 import Logo from '../components/Logo';
+
+import { BiHide, BiShowAlt } from 'react-icons/bi';
 
 import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
 
 const SignUp = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div className='min-h-screen mt-20'>
+    <div className='min-h-screen mt-20 select-none'>
       <div className='flex flex-col md:flex-row md:items-center p-3 max-w-3xl mx-auto gap-5'>
         {/* left site */}
         <div className='flex-1'>
@@ -32,16 +37,29 @@ const SignUp = () => {
             </div>
             <div className='flex flex-col gap-1'>
               <Label value='Email' />
-              <TextInput id='email' type='email' placeholder='example@email.com' required />
+              <TextInput
+                id='email'
+                type='email'
+                placeholder='example@email.com'
+                required
+              />
             </div>
             <div className='flex flex-col gap-1'>
               <Label value='Password' />
-              <TextInput
-                id='password'
-                type='password'
-                placeholder='Password'
-                required
-              />
+              <div className='relative'>
+                <TextInput
+                  id='password'
+                  type={show ? 'text' : 'password'}
+                  placeholder='Password'
+                  required
+                />
+                <span
+                  className='absolute top-2/4 right-[1px] rounded-r-sm -translate-y-2/4 p-3 bg-white inline-block cursor-pointer'
+                  onClick={() => setShow(!show)}
+                >
+                  {show ? <BiShowAlt /> : <BiHide />}
+                </span>
+              </div>
             </div>
             <Button
               className='rounded-sm hover:animate-pulse'
