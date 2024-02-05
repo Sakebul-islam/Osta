109f32bcd90588c344ed03706ad565a4cba2589c
Sakebul-islam <mdsakebul66@gmail.com>
2024-02-05 17:07:37 +0600

    compleate profile page update functionality

--- client/src/pages/Dashboard/DashboardProfile.jsx
+++ client/src/pages/Dashboard/DashboardProfile.jsx
@@ -10,15 +10,15 @@ import {
 import { app } from '../../Firebase/firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-// import {
-//   updateStart,
-//   updateSuccess,
-//   updateFailure,
-//   deleteUserStart,
-//   deleteUserSuccess,
-//   deleteUserFailure,
-//   signoutSuccess,
-// } from '../../redux/user/userSlice';
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+  // deleteUserStart,
+  // deleteUserSuccess,
+  // deleteUserFailure,
+  // signoutSuccess,
+} from '../../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
@@ -93,49 +93,49 @@ const DashboardProfile = () => {
     );
   };
 
-  // const handleChange = (e) => {
-  //   setFormData({ ...formData, [e.target.id]: e.target.value });
-  // };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   setUpdateUserError(null);
-  //   setUpdateUserSuccess(null);
-  //   if (Object.keys(formData).length === 0) {
-  //     setUpdateUserError('No changes made');
-  //     return;
-  //   }
-  //   if (imageFileUploading) {
-  //     setUpdateUserError('Please wait for image to upload');
-  //     return;
-  //   }
-  //   try {
-  //     dispatch(updateStart());
-  //     const res = await fetch(`/api/user/update/${currentUser._id}`, {
-  //       method: 'PUT',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(formData),
-  //     });
-  //     const data = await res.json();
-  //     if (!res.ok) {
-  //       dispatch(updateFailure(data.message));
-  //       setUpdateUserError(data.message);
-  //     } else {
-  //       dispatch(updateSuccess(data));
-  //       setUpdateUserSuccess("User's profile updated successfully");
-  //     }
-  //   } catch (error) {
-  //     dispatch(updateFailure(error.message));
-  //     setUpdateUserError(error.message);
-  //   }
-  // };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUpdateUserError(null);
+    setUpdateUserSuccess(null);
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError('No changes made');
+      return;
+    }
+    if (imageFileUploading) {
+      setUpdateUserError('Please wait for image to upload');
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/v1/user/update/${currentUser._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User's profile updated successfully");
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+      setUpdateUserError(error.message);
+    }
+  };
   // const handleDeleteUser = async () => {
   //   setShowModal(false);
   //   try {
   //     dispatch(deleteUserStart());
-  //     const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+  //     const res = await fetch(`/api/v1/user/delete/${currentUser._id}`, {
   //       method: 'DELETE',
   //     });
   //     const data = await res.json();
@@ -151,7 +151,7 @@ const DashboardProfile = () => {
 
   // const handleSignout = async () => {
   //   try {
-  //     const res = await fetch('/api/user/signout', {
+  //     const res = await fetch('/api/v1/user/signout', {
   //       method: 'POST',
   //     });
   //     const data = await res.json();
@@ -167,7 +167,7 @@ const DashboardProfile = () => {
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <input
           type='file'
           accept='image/*'
@@ -218,14 +218,21 @@ const DashboardProfile = () => {
           id='username'
           placeholder='username'
           defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <TextInput
           type='email'
           id='email'
           placeholder='email'
           defaultValue={currentUser.email}
+          onChange={handleChange}
         />
-        <TextInput type='password' id='password' placeholder='password' />
+        <TextInput
+          type='password'
+          id='password'
+          placeholder='password'
+          onChange={handleChange}
+        />
         <Button
           type='submit'
           gradientDuoTone='purpleToBlue'
