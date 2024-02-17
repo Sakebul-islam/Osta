fed9079f065cc274374faeb7b3fd84757890fd4a
Sakebul-islam <mdsakebul66@gmail.com>
2024-02-17 18:49:21 +0600

    get users api route create successfully

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   updateUser,
   deleteUser,
   signoutUser,
+  getUsers,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signoutUser);
+router.get('/getusers', verifyToken, getUsers);
 
 export default router;
