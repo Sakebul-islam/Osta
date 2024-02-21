a7e3455b25fb22b9e9bc8129220a01f6d2a8aa59
Sakebul-islam <mdsakebul66@gmail.com>
2024-02-21 12:30:34 +0600

    add dashboard overview to the admin dashboard

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,231 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  HiAnnotation,
+  HiArrowNarrowUp,
+  HiDocumentText,
+  HiOutlineUserGroup,
+} from 'react-icons/hi';
+import { Button, Table } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [users, setUsers] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [totalComments, setTotalComments] = useState(0);
+  const [lastMonthUsers, setLastMonthUsers] = useState(0);
+  const [lastMonthPosts, setLastMonthPosts] = useState(0);
+  const [lastMonthComments, setLastMonthComments] = useState(0);
+  const { currentUser } = useSelector((state) => state.user);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch('/api/v1/user/getusers?limit=5');
+        const data = await res.json();
+        if (res.ok) {
+          setUsers(data.users);
+          setTotalUsers(data.totalUsers);
+          setLastMonthUsers(data.lastMonthUsers);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch('/api/v1/post/getposts?limit=5');
+        const data = await res.json();
+        if (res.ok) {
+          setPosts(data.posts);
+          setTotalPosts(data.totalPosts);
+          setLastMonthPosts(data.lastMonthPosts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    const fetchComments = async () => {
+      try {
+        const res = await fetch('/api/v1/comment/getcomments?limit=5');
+        const data = await res.json();
+        if (res.ok) {
+          setComments(data.comments);
+          setTotalComments(data.totalComments);
+          setLastMonthComments(data.lastMonthComments);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchUsers();
+      fetchPosts();
+      fetchComments();
+    }
+  }, [currentUser]);
+  return (
+    <div className='p-3 md:mx-auto'>
+      <div className='flex-wrap flex gap-4 justify-center'>
+        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-sm shadow-md'>
+          <div className='flex justify-between'>
+            <div className=''>
+              <h3 className='text-gray-500 text-md uppercase'>Total Users</h3>
+              <p className='text-2xl'>{totalUsers}</p>
+            </div>
+            <HiOutlineUserGroup className='bg-teal-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+          </div>
+          <div className='flex  gap-2 text-sm'>
+            <span className='text-green-500 flex items-center'>
+              <HiArrowNarrowUp />
+              {lastMonthUsers}
+            </span>
+            <div className='text-gray-500'>Last month</div>
+          </div>
+        </div>
+        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-sm shadow-md'>
+          <div className='flex justify-between'>
+            <div className=''>
+              <h3 className='text-gray-500 text-md uppercase'>
+                Total Comments
+              </h3>
+              <p className='text-2xl'>{totalComments}</p>
+            </div>
+            <HiAnnotation className='bg-indigo-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+          </div>
+          <div className='flex  gap-2 text-sm'>
+            <span className='text-green-500 flex items-center'>
+              <HiArrowNarrowUp />
+              {lastMonthComments}
+            </span>
+            <div className='text-gray-500'>Last month</div>
+          </div>
+        </div>
+        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-sm shadow-md'>
+          <div className='flex justify-between'>
+            <div className=''>
+              <h3 className='text-gray-500 text-md uppercase'>Total Posts</h3>
+              <p className='text-2xl'>{totalPosts}</p>
+            </div>
+            <HiDocumentText className='bg-lime-600  text-white rounded-full text-5xl p-3 shadow-lg' />
+          </div>
+          <div className='flex  gap-2 text-sm'>
+            <span className='text-green-500 flex items-center'>
+              <HiArrowNarrowUp />
+              {lastMonthPosts}
+            </span>
+            <div className='text-gray-500'>Last month</div>
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-wrap gap-4 py-3 mx-auto justify-center'>
+        <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-sm dark:bg-gray-800'>
+          <div className='flex justify-between  p-3 text-sm font-semibold'>
+            <h1 className='text-center p-2'>Recent users</h1>
+            <button
+              type='button'
+              className='group hover:animate-pulse flex items-center justify-center p-0.5 text-center font-medium relative focus:z-10 focus:outline-none text-white bg-gradient-to-br from-purple-600 to-cyan-500 enabled:hover:bg-gradient-to-bl focus:ring-cyan-300 dark:focus:ring-cyan-800 border-0 focus:ring-2 rounded-sm'
+            >
+              <span className='items-center flex justify-center bg-white text-gray-900 transition-all duration-75 ease-in group-enabled:group-hover:bg-opacity-0 group-enabled:group-hover:text-inherit dark:bg-gray-900 dark:text-white w-full rounded-sm text-sm px-3 py-1.5 border border-transparent'>
+                <Link to={'/dashboard/users'}>See all</Link>
+              </span>
+            </button>
+          </div>
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell>User image</Table.HeadCell>
+              <Table.HeadCell>Username</Table.HeadCell>
+            </Table.Head>
+            {users &&
+              users.map((user) => (
+                <Table.Body key={user._id} className='divide-y'>
+                  <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <Table.Cell>
+                      <img
+                        src={user.profilePicture}
+                        alt='user'
+                        className='w-10 h-10 rounded-full bg-gray-500'
+                      />
+                    </Table.Cell>
+                    <Table.Cell>{user.username}</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              ))}
+          </Table>
+        </div>
+        <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-sm dark:bg-gray-800'>
+          <div className='flex justify-between  p-3 text-sm font-semibold'>
+            <h1 className='text-center p-2'>Recent comments</h1>
+            <button
+              type='button'
+              className='group hover:animate-pulse flex items-center justify-center p-0.5 text-center font-medium relative focus:z-10 focus:outline-none text-white bg-gradient-to-br from-purple-600 to-cyan-500 enabled:hover:bg-gradient-to-bl focus:ring-cyan-300 dark:focus:ring-cyan-800 border-0 focus:ring-2 rounded-sm'
+            >
+              <span className='items-center flex justify-center bg-white text-gray-900 transition-all duration-75 ease-in group-enabled:group-hover:bg-opacity-0 group-enabled:group-hover:text-inherit dark:bg-gray-900 dark:text-white w-full rounded-sm text-sm px-3 py-1.5 border border-transparent'>
+                <Link to={'/dashboard/comments'}>See all</Link>
+              </span>
+            </button>
+          </div>
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell className='!rounded-sm'>
+                Comment content
+              </Table.HeadCell>
+              <Table.HeadCell className='!rounded-sm'>Likes</Table.HeadCell>
+            </Table.Head>
+            {comments &&
+              comments.map((comment) => (
+                <Table.Body key={comment._id} className='divide-y'>
+                  <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <Table.Cell className='w-96'>
+                      <p className='line-clamp-2'>{comment.content}</p>
+                    </Table.Cell>
+                    <Table.Cell>{comment.numberOfLikes}</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              ))}
+          </Table>
+        </div>
+        <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-sm dark:bg-gray-800'>
+          <div className='flex justify-between  p-3 text-sm font-semibold'>
+            <h1 className='text-center p-2'>Recent posts</h1>
+            <button
+              type='button'
+              className='group hover:animate-pulse flex items-center justify-center p-0.5 text-center font-medium relative focus:z-10 focus:outline-none text-white bg-gradient-to-br from-purple-600 to-cyan-500 enabled:hover:bg-gradient-to-bl focus:ring-cyan-300 dark:focus:ring-cyan-800 border-0 focus:ring-2 rounded-sm'
+            >
+              <span className='items-center flex justify-center bg-white text-gray-900 transition-all duration-75 ease-in group-enabled:group-hover:bg-opacity-0 group-enabled:group-hover:text-inherit dark:bg-gray-900 dark:text-white w-full rounded-sm text-sm px-3 py-1.5 border border-transparent'>
+                <Link to={'/dashboard/posts'}>See all</Link>
+              </span>
+            </button>
+          </div>
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+            </Table.Head>
+            {posts &&
+              posts.map((post) => (
+                <Table.Body key={post._id} className='divide-y'>
+                  <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <Table.Cell>
+                      <img
+                        src={post.image}
+                        alt='user'
+                        className='w-14 h-10 rounded-sm bg-gray-500'
+                      />
+                    </Table.Cell>
+                    <Table.Cell className='w-96'>{post.title}</Table.Cell>
+                    <Table.Cell className='w-5'>{post.category}</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              ))}
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
