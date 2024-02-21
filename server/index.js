368f50d6759dbf446a5b2c9c49c1ddc76c9b5bb4
Sakebul-islam <mdsakebul66@gmail.com>
2024-02-21 16:28:08 +0600

    add pre installation

--- server/index.js
+++ server/index.js
@@ -1,47 +1,50 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config();
-import cookieParser from 'cookie-parser';
-import path from 'path';
-
-const port = process.env.PORT || 3000;
-
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-
 // API Routes
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
-const run = async () => {
-  await mongoose.connect(
+const port = process.env.PORT || 3000;
+dotenv.config();
+
+mongoose
+  .connect(
     `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@small-blog.1wwj7up.mongodb.net/?retryWrites=true&w=majority`
-  );
-  console.log('MongoBD is Connected');
-};
+  )
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const _dirname = path.resolve();
-run().catch((err) => console.log(err));
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-  res.json({ message: 'API connection stablish successful ✔' });
-});
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/comment', commentRoutes);
+
 app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.get('*', (req, res) => {
-  res.sendFile(__dirname, 'client', 'dist', 'index.html');
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
